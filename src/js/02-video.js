f37c2fb77c507cc25aa30fb1aa0d76cbd84a78ba
naliworld <123972576+naliworld@users.noreply.github.com>
2023-07-13 09:40:04 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const currentvideo = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const getCurrentTime = function (currentTime) {
+    localStorage.setItem(currentvideo, JSON.stringify(currentTime.seconds));
+};
+
+player.on('timeupdate', throttle(getCurrentTime,1000));
+
+const CurrentTime = localStorage.getItem(currentvideo);
+player.CurrentTime(JSON.parse(CurrentTime) || 0);
