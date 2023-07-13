b36edbdf24ee405fadbbb5745800f090297f570b
naliworld <123972576+naliworld@users.noreply.github.com>
2023-07-13 09:03:15 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryImages = document.querySelector("ul.gallery");
+galleryImages.style.listStyle = 'none';
 
+const markup = galleryItems.reduce(
+    (acc,{original, preview, description}) => 
+    (acc += `<li>
+    <a class="gallery__item" href="${original}">
+    <img 
+    class= "gallery__image"
+    src="${preview}"
+    alt="${description}"/>
+    </a>
+    </li>`),
+    ''
+);
+galleryImages.insertAdjacentElement('beforeend', markup);
+let lightbox = new SimpleLightbox (".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+});
 console.log(galleryItems);
